f0e92c9215b8212b90fa991eb5f8f32d1b0c0160
therishabhmittal05 <rishabhmittal928@gmail.com>
2024-06-17 16:39:21 +0530

    FastApi done

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,8 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://shrihari:shrihari@62.171.138.241:5432/bet_ecommerce"
+load_dotenv()
+db_user = os.getenv("DBUSER")
+db_pass = os.getenv("DBPASS")
+SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_pass}@62.171.138.241:5432/bet_ecommerce"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
