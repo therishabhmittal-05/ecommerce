789ae5662387878d65c9267d4b17521338396291
therishabhmittal05 <rishabhmittal928@gmail.com>
2024-06-17 15:53:48 +0530

    FastApi done

--- Backend/app.py
+++ Backend/app.py
@@ -1,5 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
+
+class User(BaseModel):
+    name: str
+    age : int
+    id: int 
+
+user = User(name="Rishabh", age=20, id = 1)
+
+@app.get("/")
+async def users(user: User):
+    return {"user": user}
+
+
 @app.get("/root")
 async def read_root():
     return {"Hello": "World"}
