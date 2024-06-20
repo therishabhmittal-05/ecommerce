735ed043985f687ef82cd97662aef3d835f101a3
therishabhmittal05 <rishabhmittal928@gmail.com>
2024-06-20 13:54:34 +0530

    FastApi done

--- Backend/app.py
+++ Backend/app.py
@@ -9,16 +9,16 @@ class User(BaseModel):
 
 user = User(name="Rishabh", age=20, id = 1)
 
-@app.get("/")
-async def users(user: User):
-    return {"user": user}
+# @app.post("/")
+# async def users(user: User):
+#     return {"user": user}
 
 
-@app.get("/root")
-async def read_root():
+@app.get("/products")
+async def read_products():
     return {"Hello": "World"}
 
+@app.get("/products/{product_id}")
+async def read_item(product_id: int, item: str = None):
+    return {"product_id": product_id, "item": item} 
 
-@app.get("/home")
-async def read_home():
-    return {"Hello": "Home"}
